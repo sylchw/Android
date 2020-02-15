f6435641387073618b1a68ab5df6d97ef763a3a0
sylchw <chun.hongwei@u.nus.edu>
2020-02-15 13:20:40 -0800

    fix activity main typo

--- app/src/main/java/com/example/trippin/MainActivity.java
+++ app/src/main/java/com/example/trippin/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_confirmed_transport_decision);
+        setContentView(R.layout.activity_main);
     }
 }
